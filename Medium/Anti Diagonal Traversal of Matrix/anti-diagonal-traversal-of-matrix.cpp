249e1091ae32b388141a1106345f1d93fe35556a
Appari Sai Sanjay <84316748+SanjayAppari@users.noreply.github.com>
2023-12-27 10:26:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Anti Diagonal Traversal of Matrix/anti-diagonal-traversal-of-matrix.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> antiDiagonalPattern(vector<vector<int>> matrix) 
+    {
+        int i,j,n=matrix.size();
+        vector<int>adj[n*n],ans;
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                adj[i+j].push_back(matrix[i][j]);
+            }
+        }
+        for(auto i:adj){
+            for(auto j:i) ans.push_back(j);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix;
+        matrix.resize(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> ans = ob.antiDiagonalPattern(matrix);
+        for (int i = 0; i < ans.size(); ++i) cout << ans[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
